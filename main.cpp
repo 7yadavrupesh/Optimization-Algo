089bf0de9192f94f31b0dce2f2350b2918ee5fc7
7yadavrupesh <7yadavrupesh@gmail.com>
2016-08-23 17:25:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main()
 {
-    Population pop(Dna("This is rupesh yadav"),.5);
-    pop.initializePopulation(10000);
+    Population pop(Dna("This is rupesh yadav"));
+    pop.setMutationRate(.1);
+    pop.initializePopulation(1000000);
     //pop.printPopulation();
-    for(int i=0; i<10; i++){
+    for(int i=0; i<5; i++){
         pop.calFitness();
         if(pop.isFinished() == true)
             break;
